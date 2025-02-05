856e42f95282a4154e2be2bf97e23b5f6e6b147c
waellllll <wael.khalfi@esprit.tn>
2025-02-05 18:12:40 +0100

    Template
    Template

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomeController extends AbstractController
+{
+    #[Route('/home', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('baseback.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+    #[Route('/homeb', name: 'app_homeb')]
+    public function indexb(): Response
+    {
+        return $this->render('base.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
