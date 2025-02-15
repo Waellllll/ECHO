a372a2ff9d317dee9e9df0c30cba88c24d808d92
waellllll <wael.khalfi@esprit.tn>
2025-02-15 17:25:15 +0100

    integration
    integration

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{   
+    
+    #[Route(path: '/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // Check if the user is already authenticated
+        if ($this->getUser()) {
+            // Get the authenticated user
+            $user = $this->getUser();
+            
+            // Redirect based on the user's role
+            if (in_array('ROLE_ADMIN', $user->getRoles(), true)) {
+                return $this->redirectToRoute('homeback'); // Redirect to Back-Office for admin
+            }
+            
+            // Redirect to Front-Office for regular users
+            return $this->redirectToRoute('home'); 
+        }
+    
+        // Retrieve any authentication error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // Get the last username entered
+        $lastUsername = $authenticationUtils->getLastUsername();
+    
+        // Render the login form with the last username and error message
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername, 
+            'error' => $error
+        ]);
+    }
+    
+   
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+}
---
+++ src/Form/UserFormType.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use App\Enum\UserType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
+use Symfony\Component\Validator\Constraints\EqualTo;
+
+
+class UserFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class)
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe',
+                'required' => true,
+                'attr' => ['autocomplete' => 'new-password'],
+            ])
+            
+            ->add('roles', ChoiceType::class, [
+                'choices'  => [
+                    'Admin' => 'ROLE_ADMIN',
+                    'Agriculteur Professionnel' => 'ROLE_PROFESSIONNEL',
+                    'Agriculteur Amateur' => 'ROLE_AMATEUR',
+                ],
+                'expanded' => true,
+                'multiple' => true,
+            ])
+            ->add('typeUtilisateur', ChoiceType::class, [
+                'choices'  => array_combine(
+                    array_map(fn(UserType $type) => $type->label(), UserType::cases()),
+                    UserType::cases()
+                ),
+                'choice_value' => fn (?UserType $type) => $type?->value,
+                'expanded' => false,
+                'multiple' => false,
+                'required' => true,
+                'placeholder' => 'Sélectionnez un type d utilisateur',
+            ]); 
+
+    } 
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
